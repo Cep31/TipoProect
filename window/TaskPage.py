2c38857e4525982068b00c1709896d16650f0e48
Cep31 <cepbl31@yandex.ru>
2025-10-22 21:02:57 +0300

    описание в прошлом комите, оно че-то не захотело в прошлый раз комитить эти изменения

--- window/TaskPage.py
+++ window/TaskPage.py
@@ -1,30 +1,28 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import PhotoImage
 from object.Task import Task
 
-answers1=[]
+answers=[[],[],[],[],[],[],[],[],[],[],[],[]]#делаю матрицу в которой будут ответы пользователя на задачи
 
 class TaskPage(tk.Frame):
     def __init__(self, root, controller, tasks_id):
         super().__init__(root)
-        # Task - это данные задания
-        self.tasks_id = tasks_id
-        self.tasks = []
         self.controller = controller
         for task in tasks_id:
             self.tasks.append(Task(task))
-            self.root = root
-            self.root.title("Задачи")
-            self.root.geometry("800x1200")
+        self.root = root
+        self.root.title("Задачи")
+        self.root.geometry("800x1200")
 
-            self.style = ttk.Style()
-            self.setup_tab_styles()
+        self.style = ttk.Style()
+        self.setup_tab_styles()
 
-            self.notebook = ttk.Notebook(root)
-            self.notebook.pack(fill='both', expand=True, padx=10, pady=10)
+        self.notebook = ttk.Notebook(root)
+        self.notebook.pack(fill='both', expand=True, padx=10, pady=10)
 
-            self.create_tab_1()#создаем все табы, оно у меня не заработало на классах, поэтому все в таск запихну
-            self.create_last_tab()
+        self.create_tab_1()  # создаем все табы, оно у меня не заработало на классах, поэтому все в таск запихну
+        self.create_last_tab()
 
     def setup_tab_styles(self):
         # Создаем общий стиль для всех вкладок с цветами
@@ -34,7 +32,8 @@ class TaskPage(tk.Frame):
 
     def create_tab_1(self):#создание содержимого
             exercises1 = ["какая-то задача из первого", "другая задача из первого", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28"]
-            for i in range():
+            answer1=[]
+            for i in range(2):
                 tab = ttk.Frame(self.notebook)
 
                 self.notebook.add(tab, text={i + 1})
@@ -42,10 +41,10 @@ class TaskPage(tk.Frame):
                 zadanie_frame = ttk.Frame(tab)
                 zadanie_frame.pack(fill='both', expand=True, padx=10, pady=10)
 
-                zadanie = tk.Text(zadanie_frame, font=('Arial', 12), height=15, wrap='word')  # делаем текст задачи
-                zadanie.pack(fill='both', expand=True)
-                zadanie.insert('1.0', exercises1[i])
-                zadanie.config(state='disabled')
+                gp = Task()#я постарался разобраться как фуекцию вызвать чтоб фото взять, че-то сделал
+                zadanie = gp.get_photo()
+                image_label = ttk.Label(zadanie_frame, image=zadanie)
+                image_label.pack()
 
                 answer_frame = ttk.Frame(tab)
                 answer_frame.pack(fill='x', padx=10, pady=10)
@@ -57,45 +56,27 @@ class TaskPage(tk.Frame):
                 answer.pack(side='left', fill='x', expand=True, padx=(0, 10))
 
                 otvet = ttk.Button(answer_frame, text="Ответить",
-                                   command=lambda idx=i, ans=answer: self.get_answer(idx, ans))  # ответ
+                                   command=lambda idx=i, ans=answer: self.get_answer1(idx, ans))  # ответ
                 otvet.pack(side='right')
-    def get_answer(self, task_index, answer):#получаем отвтет
+    def get_answer1(self, task_index, answer):#получаем отвтет
         user_answer = answer.get()
 
         if not user_answer:
             # result_label.config(text="Введите ответ!")
             return
-        answers1.append(user_answer)
+        answers[0].append(user_answer)
+
     def create_last_tab(self):
         tab = ttk.Frame(self.notebook)
 
-        self.notebook.add(tab, text={"Закончить"})
+        self.notebook.add(tab, text="Закончить")
 
         last_frame = ttk.Frame(tab)
         last_frame.pack(fill='both', expand=True, padx=10, pady=10)
 
-        zaver=ttk.Button(last_frame, text="Хотите закончить?", command=lambda: self.show_tocho())
-        zaver.pack(side='left', padx=(0, 10))
+        #zaver=ttk.Button(last_frame, text="Хотите закончить?", command=lambda: self.show_tocho())
+        #zaver.pack(side='left', padx=(0, 10))
 
-    def show_tocho(selfl, rootl):#создаем новое окно
-        selfl.root = rootl
-        selfl.root.title("")
-        selfl.root.geometry("200x300")
-
-        frame = ttk.Frame(rootl)
-        frame.pack(fill='both', expand=True, padx=10, pady=10)
-
-        label=ttk.Label(frame, text="Вы точно хотите завершить?", font=('Arial', 12))
-        label.pack(side='top', padx=(0, 10))
-
-        da=ttk.Button(frame, text="завершить")#уничтожить страницу в которой задания и создать с решениями
-        da.pack(side='right', padx=10, pady=10)
-
-        net=ttk.Button(frame, text="подумаю еще", command=lambda: selfl.drop_last_tab())#уничтожить страницу в которой задания и создать с решениями
-        net.pack(side='left', padx=10, pady=10)
-
-    def drop_last_tab(selfl, rootl):#уничтожает окно с завершить
-        rootl.destroy()
 
 
 # root = tk.Tk()
